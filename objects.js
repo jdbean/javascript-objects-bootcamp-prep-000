6975437d7fbe9eda5f7a68728a710e68a73f711b
J.D. Bean <jonathandbean@gmail.com>
2017-11-29 23:52:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
 var playlist = {};
 
-function updatePlaylist(list, artistName, songTitle) {
+function updatePlaylist(playlist, artistName, songTitle) {
   playlist.artistName = songTitle
   return playlist
 }
 
-function removeFromPlaylist(list, artistName) {
+function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName
   return playlist
 }
